1e405375bd3284951ffffd56cdf49741e21d9943
Pranav c Dipu <pranavmj3000@gmail.com>
2023-09-20 17:58:26 +0530

    from dif lap

--- uploadImage.js
+++ uploadImage.js
@@ -7,43 +7,56 @@ exports.handler = async (event) => {
     try {
 
         const bucket_name = process.env.AWS_S3_BUCKET_NAME;
-        const base64Image = event.body; // The base64-encoded image data sent from the web app
-        const contentType = event.headers['Content-Type']; // Get the content type from headers
+        const base64Image = event.body; 
+        const contentType = event.headers['Content-Type'];
 
-        // Create a unique key for the S3 object (you can modify this as needed)
+      
         const key = `images/${Date.now()}.${getFormatFromContentType(contentType)}`;
 
-        // Decode the base64 image to binary data
+      
         const imageBuffer = Buffer.from(base64Image, 'base64');
 
-        // Define S3 upload parameters
+       
         const params = {
-            Bucket: bucket_name, // Replace with your S3 bucket name
+            Bucket: bucket_name, 
             Key: key,
             Body: imageBuffer,
-            ContentType: contentType, // Use the provided content type
+            ContentType: contentType, 
             
         };
 
-        // Upload the image to S3
+      
         await s3.putObject(params).promise();
 
-        // Respond with a success message
         const response = {
             statusCode: 200,
             body: JSON.stringify('Image uploaded successfully'),
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+                'Access-Control-Allow-Headers':
+                  'Content-Type,X-Amz-Date,X-Api-Key,X-Amz-Security-Token,Authorization,Content-Length,X-Request-With',
+                'Access-Control-Allow-Credentials': true
+              }
         };
         return response;
     } catch (error) {
         console.error('Error:', error);
         return {
             statusCode: 500,
-            body: JSON.stringify('Image upload failed'),
+            body: JSON.stringify('Image upload failed'),headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+                'Access-Control-Allow-Headers':
+                  'Content-Type,X-Amz-Date,X-Api-Key,X-Amz-Security-Token,Authorization,Content-Length,X-Request-With',
+                'Access-Control-Allow-Credentials': true
+              }
         };
     }
 };
 
-// Function to extract the image format from the content type
 function getFormatFromContentType(contentType) {
     switch (contentType) {
         case 'image/jpeg':
@@ -53,6 +66,6 @@ function getFormatFromContentType(contentType) {
         case 'image/gif':
             return 'gif';
         default:
-            return 'jpg'; // Default to JPEG if content type is unknown
+            return 'jpg'; 
     }
 }
